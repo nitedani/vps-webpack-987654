7931ee3b5c401a53614bac9aaaff496278ec3e7e
Dániel Horváth <horvath.daniel@lexunit.hu>
2023-04-14 16:21:05 +0200

    remove vavite

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,5 @@
 import ssr from "vite-plugin-ssr/plugin";
-import vavite from "vavite";
-import { join } from "path";
 
 export default {
-  buildSteps: [
-    { name: "client" },
-    {
-      name: "server",
-      config: {
-        build: {
-          ssr: true,
-        },
-      },
-    },
-  ],
-  plugins: [
-    ssr(),
-    vavite({
-      serverEntry: join(__dirname, "server", "main.js"),
-      serveClientAssetsInDev: true,
-    }),
-  ],
+  plugins: [ssr()],
 };
